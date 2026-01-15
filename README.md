db8b672c79d06a29c3ac23a735ffa54f8838b709
C3rb3rus2050 <C3rb3rus.2050@gmail.com>
2026-01-15 13:06:03 +0100

    changed

--- README.md
+++ README.md
@@ -6,7 +6,12 @@ Shopfloor install
 sudo apt update -y
 sudo apt-get install git -y
 sudo git clone https://github.com/C3rb3rus2050/MPDV_Shopfloor.git
-cd Proxmox_Scripts
+cd MPDV_Shopfloor
+mv MPDV_script /root/
+mv install.sh /root/
+cd
+rm -r /root/MPDV_Shopfloor
+
 sudo sh install.sh
 
 
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# System Update install dependencies
+#!/bin/bash
 
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -15,14 +15,60 @@ sudo apt install python3-dotenv
 
 apt update --fix-missing -y
 
+# --- Set locale, timezone, keyboard ---
 
-# Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl -y
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+echo "Starting DietPi configuration..."
 
-sudo rm -r Proxmox_Scripts/
+# --- 1. Set Locale to C.UTF-8 ---
+echo "Setting locale to C.UTF-8..."
+sudo locale-gen C.UTF-8
+sudo update-locale LANG=C.UTF-8
+export LANG=C.UTF-8
+echo "Locale set to:"
+locale | grep LANG
 
-reboot now
+# --- 2. Set Timezone to Europe/Vienna ---
+echo "Setting timezone to Europe/Vienna..."
+sudo timedatectl set-timezone Europe/Vienna
+echo "Timezone set to:"
+timedatectl | grep "Time zone"
+
+# --- 3. Set keyboard layout to Austrian (at) ---
+echo "Setting keyboard layout to 'at'..."
+sudo localectl set-keymap at
+sudo localectl set-x11-keymap at
+echo "Keyboard layout set to:"
+localectl status | grep "Keymap"
+
+# Apply instantly for current console
+sudo loadkeys at
+
+echo "✅ DietPi configuration completed!"
+
+CONFIG_FILE="/boot/firmware/config.txt"
+
+# Replace dtoverlay line
+sudo sed -i 's/^dtoverlay=vc4-kms-v3d/dtoverlay=vc4-fkms-v3d/' "$CONFIG_FILE"
+
+# Replace hdmi_blanking line
+sudo sed -i 's/^hdmi_blanking=1/hdmi_blanking=2/' "$CONFIG_FILE"
+
+echo "config.txt updated successfully."
+
+chmod +x /root/MPDV_script/MPDV.py
+chmod +x /root/MPDV_script/display_schedule_daily.py
+
+cd MPDV_script
+
+sudo mv display_schedule_daily.service /etc/systemd/system/
+sudo mv selenium_startup.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl enable display_schedule_daily.service
+sudo systemctl start display_schedule_daily.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable selenium_startup.service
+sudo systemctl start selenium_startup.service
+
+#reboot now
